debdf7d530ceb4ce706a03a0117112737c9e27f2
KingOscar68 <oscarlt1168@gmail.com>
2024-11-18 23:00:54 -0500

    grafica limpia

--- lib/data_raw.dart
+++ lib/data_raw.dart
@@ -12,98 +12,88 @@ class DataRawPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text("ECG1 Plotter")),
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text("ECG1 Real-Time Plot"),
-          ),
-          SizedBox(
-            height: 300,
-            child: Obx(() {
-              return LineChart(
-                LineChartData(
-                  gridData: FlGridData(
-                    show: true,
-                    drawVerticalLine: true,
-                    verticalInterval: 500, // Intervalos en eje X
-                    horizontalInterval: (1 << 21) / 5, // Intervalos en eje Y
-                    getDrawingHorizontalLine: (value) => FlLine(
-                      color: Colors.grey.shade400,
-                      strokeWidth: 0.8,
-                    ),
-                    getDrawingVerticalLine: (value) => FlLine(
-                      color: Colors.grey.shade400,
-                      strokeWidth: 0.8,
-                    ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return Center(
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "ECG1 Real-Time Plot",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  borderData: FlBorderData(
-                    show: true,
-                    border: const Border(
-                      left: BorderSide(color: Colors.black, width: 1),
-                      bottom: BorderSide(color: Colors.black, width: 1),
-                    ),
-                  ),
-                  titlesData: FlTitlesData(
-                    show: true,
-                    leftTitles: AxisTitles(
-                      sideTitles: SideTitles(
-                        showTitles: true,
-                        reservedSize: 40,
-                        interval: (1 << 21) / 5,
-                        getTitlesWidget: (value, meta) {
-                          return Text(
-                            value.toStringAsFixed(0),
-                            style: const TextStyle(fontSize: 10),
-                          );
-                        },
-                      ),
-                    ),
-                    bottomTitles: AxisTitles(
-                      sideTitles: SideTitles(
-                        showTitles: true,
-                        reservedSize: 40,
-                        interval: 500,
-                        getTitlesWidget: (value, meta) {
-                          return Text(
-                            value.toInt().toString(),
-                            style: const TextStyle(fontSize: 10),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                  lineBarsData: [
-                    LineChartBarData(
-                      spots: List.generate(
-                        controller.ecg1Data.length,
-                            (i) => FlSpot(i.toDouble(), controller.ecg1Data[i].toDouble()),
-                      ),
-                      isCurved: true,
-                      color: Colors.blue,
-                      dotData: FlDotData(show: false),
-                      belowBarData: BarAreaData(show: false),
-                    ),
-                  ],
-                  minY: -(1 << 21).toDouble(),
-                  maxY: (1 << 21).toDouble(),
-                  minX: 0,
-                  maxX: controller.ecg1Data.length.toDouble(),
                 ),
-              );
-            }),
-          ),
-          const SizedBox(height: 10),
-          Obx(() {
-            final ecg1 = controller.decodedValues["ecg1"] ?? 0;
-            return Text(
-              "Muestra actual: $ecg1",
-              style: Theme.of(context).textTheme.bodyLarge,
-            );
-          }),
-        ],
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Obx(() {
+                      return Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2), // Recuadro alrededor de la gráfica
+                          borderRadius: BorderRadius.circular(8), // Bordes redondeados opcionales
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: LineChart(
+                            LineChartData(
+                              gridData: FlGridData(
+                                show: true,
+                                drawVerticalLine: true,
+                                verticalInterval: 500,
+                                horizontalInterval: (1 << 21) / 5,
+                                getDrawingHorizontalLine: (value) => FlLine(
+                                  color: Colors.grey.shade400,
+                                  strokeWidth: 0.8,
+                                ),
+                                getDrawingVerticalLine: (value) => FlLine(
+                                  color: Colors.grey.shade400,
+                                  strokeWidth: 0.8,
+                                ),
+                              ),
+                              borderData: FlBorderData(
+                                show: false, // Oculta las líneas de los bordes
+                              ),
+                              titlesData: FlTitlesData(
+                                show: false, // Oculta las etiquetas de los ejes
+                              ),
+                              lineBarsData: [
+                                LineChartBarData(
+                                  spots: List.generate(
+                                    controller.ecg1Data.length,
+                                        (i) => FlSpot(i.toDouble(), controller.ecg1Data[i].toDouble()),
+                                  ),
+                                  isCurved: true,
+                                  color: Colors.blue,
+                                  dotData: FlDotData(show: false),
+                                  belowBarData: BarAreaData(show: false),
+                                ),
+                              ],
+                              minY: -(1 << 21).toDouble(),
+                              maxY: (1 << 21).toDouble(),
+                              minX: 0,
+                              maxX: controller.ecg1Data.length.toDouble(),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Obx(() {
+                  final ecg1 = controller.decodedValues["ecg1"] ?? 0;
+                  return Text(
+                    "Muestra actual: $ecg1",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  );
+                }),
+                const SizedBox(height: 20),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
 }
-
